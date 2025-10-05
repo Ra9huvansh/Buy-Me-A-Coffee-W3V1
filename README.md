6d5398bc8df078127cac923d17c915447abc72e4
Raghuvansh <raghuvansh.siddharth004@gmail.com>
2025-10-05 06:14:41 +0530

    added other functionalities

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # What are we making ?
 - Minimal HTML/JS site
 - That has the following buttons which map to the solidity smart contract
-    - Connect
+    - Connect 
     - Buy Coffee
+        - Button to but coffee
+        - Call a function on a smart contract
+        - Have a test function that we can call?
     - Get Balance
     - Withdraw
   
--- index-js.js
+++ index-js.js
@@ -1,8 +1,11 @@
-import { createWalletClient, custom } from 'https://esm.sh/viem'
+import { createWalletClient, custom, createPublicClient } from 'https://esm.sh/viem'
 
 const connectButton = document.getElementById('connectButton')
+const fundButton = document.getElementById('fundButton')
+const ethAmountInput = document.getElementById('ethAmountInput')
 
 let walletClient
+let publicClient
 
 async function connect() {
     if(typeof window.ethereum !== 'undefined'){
@@ -17,6 +20,30 @@ async function connect() {
     }
 }
 
+async function fund() {
+    const ethAmount = ethAmountInput.value
+    console.log(`Funding with ${ethAmount}...`)
+
+    if(typeof window.ethereum !== 'undefined'){
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        })
+        await walletClient.requestAddresses()
+        connectButton.innerText = 'Connected!'
+
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        })
+        await publicClient.simulateContract({
+            
+        })
+    }
+    else{
+        connectButton.innerText = 'Please install MetaMask!'
+    }
+}
+
 
 connectButton.onclick = connect
+fundButton.onclick = fund
 
